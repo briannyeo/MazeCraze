4a91fa74d67a2d52eb5973d11a25506fd30baacf
brianyeo <brianyeo1995@gmail.com>
2022-01-18 22:16:55 +0800

    centered and styled the timer

--- controls.js
+++ controls.js
@@ -149,12 +149,10 @@ function move(event) {
 }
 //Timer
   
-//Define vars to hold time values
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-//Define vars to hold "display" value
 let displaySeconds = 0;
 let displayMinutes = 0;
 let displayHours = 0;
@@ -175,14 +173,12 @@ function stopWatch(){
         seconds = 0;
         minutes++;
 
-        if(minutes / 60 === 1){
-            minutes = 0;
-            hours++;
-        }
+        // if(minutes / 60 === 1){
+        //     minutes = 0;
+        //     hours++;
+        // }
 
     }
-
-    //If seconds/minutes/hours are only one digit, add a leading 0 to the value
     if(seconds < 10){
         displaySeconds = "0" + seconds.toString();
     }
@@ -197,15 +193,15 @@ function stopWatch(){
         displayMinutes = minutes;
     }
 
-    if(hours < 10){
-        displayHours = "0" + hours.toString();
-    }
-    else{
-        displayHours = hours;
-    }
+    // if(hours < 10){
+    //     displayHours = "0" + hours.toString();
+    // }
+    // else{
+    //     displayHours = hours;
+    // }
 
     //Display updated time values to user
-    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+    document.getElementById("display").innerHTML =  displayMinutes + ":" + displaySeconds;
 
 }
 
@@ -215,14 +211,12 @@ function startStop(){
 
         //Start the stopwatch (by calling the setInterval() function)
         interval = window.setInterval(stopWatch, 1000);
-        //document.getElementById("startStop").innerHTML = "Stop";
         status = "started";
 
     }
     else{
 
         window.clearInterval(interval);
-        //document.getElementById("startStop").innerHTML = "Start";
         status = "stopped";
 
     }
